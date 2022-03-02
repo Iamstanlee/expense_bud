97913201e9eed8f75952ca648e1d6d8ca1f2b35f
Daniel Tolentino <danieltolentino@protonmail.com>
2022-03-02 12:35:45 -0300

    add brl coin
    Add Brazilian Real currency.

--- lib/core/utils/currencies.dart
+++ lib/core/utils/currencies.dart
@@ -3,5 +3,6 @@ import 'package:expense_bud/features/settings/domain/entities/currency.dart';
 const usd = CurrencyEntity(name: 'USD', locale: "en_US", symbol: "\u0024");
 const ngn = CurrencyEntity(name: 'NGN', locale: "en_NG", symbol: "\u20A6");
 const gbp = CurrencyEntity(name: 'GBP', locale: "en_GB", symbol: "\u00A3");
+const brl = CurrencyEntity(name: 'BRL', locale: "pt_BR", symbol: "\u0052\u0024");
 
-List<CurrencyEntity> currencyList() => [usd, gbp, ngn];
+List<CurrencyEntity> currencyList() => [usd, gbp, ngn, brl];
